4a3c01320b91e3b8e438a78916dd98c4501878c4
Victoria Demina <viktoriia.demina@gmail.com>
2023-04-22 21:23:41 +0100

    Add REST API.

---
+++ Play.Catalog.Service/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Play.Catalog.Service.Controllers;
+
+[ApiController]
+[Route("items")]
+public class ItemsController : ControllerBase
+{
+    private static readonly List<ItemDto> Items = new() 
+    {
+        new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+        new ItemDto(Guid.NewGuid(), "Antidote", "Cures potion", 7, DateTimeOffset.UtcNow),
+        new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+    };
+
+    [HttpGet]
+    public IEnumerable<ItemDto> Get()
+    {
+        return Items;
+    }
+    [HttpGet("{id}")]
+    public ItemDto GetById(Guid id)
+    {
+        var item = Items.SingleOrDefault(item => item.Id == id);
+        return item;
+    }
+
+    [HttpPost]
+    public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+    {
+        var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+        Items.Add(item);
+
+        return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+    {
+        var existedItem = Items.SingleOrDefault(item => item.Id == id);
+        var updatedItem = existedItem with{
+            Name = updateItemDto.Name,
+            Description = updateItemDto.Description,
+            Price = updateItemDto.Price
+        };
+
+        var index = Items.FindIndex(existingItem => existingItem.Id == id);
+        Items[index] = updatedItem;
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        var index = Items.FindIndex(existingItem => existingItem.Id == id);
+        Items.RemoveAt(index);
+
+        return NoContent();
+    }
+}
